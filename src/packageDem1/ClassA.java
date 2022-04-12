f9ba8d7f585020312f3ac501a0c673ad1bf9e2bb
CelAn812 <celia.anca92@gmail.com>
2022-04-11 23:07:21 -0500

    First Commit

---
+++ src/packageDem1/ClassA.java
@@ -0,0 +1,8 @@
+package packageDem1;
+
+public class ClassA {
+	public static void main(String[] args) {
+		System.out.println("Demonstration 1");
+	}
+
+}
---
+++ src/packageDem1/ClassB.java
@@ -0,0 +1,7 @@
+package packageDem1;
+
+public class ClassB {
+	public static void main(String[] args) {
+		System.out.println("Demonstration 2");
+	}
+}
